59f56b956495c3f3ccd79d711aa06379fde7c1ff
RajNalla123 <rajashekarreddy606@gmail.com>
2022-10-20 12:41:14 +0530

    mine

---
+++ src/test/java/practicemy2/myprac.java
@@ -0,0 +1,11 @@
+package practicemy2;
+
+public class myprac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my practcicvk");
+System.out.println("nxnmx");
+	}
+
+}
---
+++ src/test/java/practicemy3/Practicemine3.java
@@ -0,0 +1,10 @@
+package practicemy3;
+
+public class Practicemine3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
