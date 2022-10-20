6f1b7042d27b7c930ad94b5218da22de7879f525
RajNalla123 <rajashekarreddy606@gmail.com>
2022-10-20 11:49:11 +0530

    how

---
+++ src/test/java/demogit/Gitagain2.java
@@ -0,0 +1,13 @@
+package demogit;
+
+public class Gitagain2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=123;
+		double s=22.2;
+		System.out.println("gitbash a,s");
+
+	}
+
+}
